ae71954b85b34215d421462c6be8aa772067e7aa
jigar chovatiya <115682188+Jigarchovatiya@users.noreply.github.com>
2023-06-08 19:46:29 +0530

    08-06-2023

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,4 +1,18 @@
 import 'package:blood_donation_app/utils/routes/routes_name.dart';
+import 'package:blood_donation_app/view/bottom_bar/bottom_bar_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/campaign/campaigns_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/donnors/donnors_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/donnors/donors_detail_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/history/history_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/home_screen/donation_campaign_four.dart';
+import 'package:blood_donation_app/view/bottom_bar/home_screen/donation_campaign_one.dart';
+import 'package:blood_donation_app/view/bottom_bar/home_screen/donation_campaign_three.dart';
+import 'package:blood_donation_app/view/bottom_bar/home_screen/donation_campaign_two.dart';
+import 'package:blood_donation_app/view/bottom_bar/home_screen/home_screen.dart';
+import 'package:blood_donation_app/view/bottom_bar/profile/profile_screen.dart';
+import 'package:blood_donation_app/view/entry_point/create_account_one.dart';
+import 'package:blood_donation_app/view/entry_point/create_account_two.dart';
+import 'package:blood_donation_app/view/entry_point/log_in_screen.dart';
 import 'package:blood_donation_app/view/onboarding/onboarding_screen.dart';
 import 'package:blood_donation_app/view/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -6,18 +20,18 @@ import 'package:flutter/material.dart';
 Map<String, WidgetBuilder> appRoutes = {
   RoutesName.splashScreen: (context) => const SplashScreen(),
   RoutesName.onBoarding: (context) => const OnBoardingScreen(),
-  // RoutesName.pageOne: (context) => const PageOne(),
-  // RoutesName.pageTwo: (context) => const PageTwo(),
-  // RoutesName.pageThree: (context) => const PageThree(),
-  // RoutesName.logInScreen: (context) => const LogInScreen(),
-  // RoutesName.continueWithScreen: (context) => const ContinueWithScreen(),
-  // RoutesName.otpLogInScreen: (context) => const OtpLogInScreen(),
-  // RoutesName.personalizationScreen: (context) => const PersonalizationScreenOne(),
-  // RoutesName.emptyCart: (context) => const EmptyCart(),
-  // RoutesName.home: (context) => const Home(),
-  // RoutesName.favStore: (context) => const FavStore(),
-  // RoutesName.cart: (context) => const Cart(),
-  // RoutesName.profile: (context) => const Profile(),
-  // RoutesName.bottomBar: (context) => const BottomBar(),
-  // RoutesName.section: (context) => const Section(),
+  RoutesName.logInScreen: (context) => const LogInScreen(),
+  RoutesName.createAccountOne: (context) => const CreateAccountOne(),
+  RoutesName.createAccountTwo: (context) => const CreateAccountTwo(),
+  RoutesName.bottomNavBar: (context) => const BottomBarScreen(),
+  RoutesName.home: (context) => const HomeScreen(),
+  RoutesName.donationCampaignOne: (context) => const DonationCampaignOne(),
+  RoutesName.donationCampaignTwo: (context) => const DonationCampaignTwo(),
+  RoutesName.donationCampaignThree: (context) => const DonationCampaignThree(),
+  RoutesName.donationCampaignFour: (context) => const DonationCampaignFour(),
+  RoutesName.donor: (context) => const DonnerScreen(),
+  RoutesName.donorDetail: (context) => const DonorsDetailScreen(),
+  RoutesName.campaign: (context) => const CampaignsScreen(),
+  RoutesName.history: (context) => const HistoryScreen(),
+  RoutesName.profile: (context) => const ProfileScreen(),
 };
--- lib/utils/routes/routes_name.dart
+++ lib/utils/routes/routes_name.dart
@@ -1,18 +1,18 @@
 class RoutesName {
   static const String splashScreen = "/";
   static const String onBoarding = "/onBoarding";
-  // static const String pageOne = "/pageOne";
-  // static const String pageTwo = "/pageTwo";
-  // static const String pageThree = "/pageThree";
-  // static const String logInScreen = "/logInScreen";
-  // static const String continueWithScreen = "/continueWithScreen";
-  // static const String otpLogInScreen = "/otpLogInScreen";
-  // static const String personalizationScreen = "/personalizationScreen";
-// static const String emptyCart = "/emptyCart";
-// static const String bottomBar = "/bottomBar";
-// static const String home = "/home";
-// static const String favStore = "/favStore";
-// static const String cart = "/cart";
-// static const String profile = "/profile";
-// static const String section = "/section";
+  static const String logInScreen = "/logInScreen";
+  static const String createAccountOne = "/createAccountOne";
+  static const String createAccountTwo = "/createAccountTwo";
+  static const String bottomNavBar = "/bottomNavBar";
+  static const String home = "/home";
+  static const String donationCampaignOne = "/donationCampaignOne";
+  static const String donationCampaignTwo = "/donationCampaignTwo";
+  static const String donationCampaignThree = "/donationCampaignThree";
+  static const String donationCampaignFour = "/donationCampaignFour";
+  static const String donor = "/donor";
+  static const String donorDetail = "/donorDetail";
+  static const String campaign = "/campaign";
+  static const String history = "/history";
+  static const String profile = "/profile";
 }
